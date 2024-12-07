7c64c4a3fccc2ca5065bf2d03d6e823f8fb448c5
Craig <c.matear@gmail.com>
2024-12-07 19:59:51 +0000

    adding sync thing

--- setup/install_tools.sh
+++ setup/install_tools.sh
@@ -4,6 +4,8 @@ sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /et
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
 sudo dpkg --add-architecture i386 # for steam
 
 sudo apt update
@@ -17,6 +19,12 @@ sudo apt install -y \
     virtinst \
     libvirt-clients \
     bridge-utils \
-    libvirt-daemon-system
+    libvirt-daemon-system \
+    bspwm \
+    polybar \
+    rofi \
+    feh \
+    terminator\
+    syncthing
 
 snap install discord
